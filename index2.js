03ce15e5353c92345349a90f1d676e84ce8b67c5
PriyankaJadhavar <priyanka.jadhavar@communitybrands.com>
2023-11-27 12:41:38 +0530

    Database Working Code

--- index2.js
+++ index2.js
@@ -1,12 +1,16 @@
-import { OpenAI } from "langchain";
-import { SqlDatabase } from "langchain/dist/sql_db";
-import { createSqlAgent, SqlToolkit } from "langchain/agents";
-import { DataSource } from 'typeorm';
+import { OpenAI } from "langchain/llms/openai";
+import { SqlDatabase } from "langchain/sql_db";
+import { createSqlAgent, SqlToolkit } from "langchain/agents/toolkits/sql";
+import { DataSource } from "typeorm";
+
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
 
 const run = async () => {
     const datasource = new DataSource({
-        type: "mysql",
-        database: "./Chinook_MySql"
+        type: "sqlite",
+        database: "database/Chinook_MySql"
     });
     const db = await SqlDatabase.fromDataSourceParams({
         appDataSource: datasource,
@@ -19,11 +23,19 @@ const run = async () => {
 
     const executor = createSqlAgent(model, toolkit);
 
-    const input = 'SQL Query';
+    const input = 'List all Album.';
 
-    console.log('Input-> "${input}"');
+    console.log('Input->', {input});
 
     const result = await executor.call({ input });
+
+    console.log('Result ->', {result});
+
+    console.log(`Intermediate Steps: ${JSON.stringify(
+        result.intermediateSteps,
+        null,
+        2
+    )}`);
 
     await datasource.destroy();
 };
